549894fbebd6928463e76d0e749ef94e7ac70f24
annebrt <153097284+annebrt@users.noreply.github.com>
2026-01-11 03:49:06 +0200

    Create Admin.h

---
+++ admin.h
@@ -0,0 +1,17 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+
+#include "BaseUtilizator.h"
+#include "BaseVideo.h"
+
+class Admin:public BaseUtilizator{
+
+    public:
+
+    void deleteVideo(BaseVideo* video){
+
+    }
+
+};
+
+#endif // ADMIN_H
