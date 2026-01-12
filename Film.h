36893b323910f60d1bfea24b669ce8f560077ead
annebrt <153097284+annebrt@users.noreply.github.com>
2026-01-12 05:38:25 +0200

    Update Film.h

--- Film.h
+++ Film.h
@@ -12,20 +12,62 @@ class Film: public BaseVideo{
         
         Durata* durata; 
         int buget;
+
+    void afisare(std::ostream& out) const override {
+
+        BaseVideo::afisare(out);
+
+        out<<"durata: "<<*durata;
+        out<<"buget"<<buget;
+
+    }
+
+    void citire(std::istream& in) override{
+
+        BaseVideo::citire(in);
+
+        int ore, minute,secunde;
+
+        std::cout<<"ore: ";
+        in>>ore;
+        std::cout<<"minute: ";
+        in>>minute;
+        std::cout<<"secunde: ";
+        in>>secunde;
+
+        std::cout<<"buget: ";
+        in>>this->buget;
+
+        delete this->durata;
+
+        Durata* durata= new Durata(ore,minute,secunde);
+
+        this->durata=durata;
+
+    }
     
     public: 
 
+        Film(){
+            this->durata=nullptr;
+        }
+
         Film(const char* denumire, std::list<std::string> cast, const char* genre, const char* descriere, Durata* durata, int buget):BaseVideo(denumire,cast,genre,descriere)
-            ,durata(durata),buget(buget){}
+            ,buget(buget) {
+
+
+                this->durata= new Durata(*durata);
+
+            }
 
         ~Film(){
             delete  durata;
         }
 
-    void afisare(std::ostream& out) {
+    
 
+    
 
-    }
     
 };
 
