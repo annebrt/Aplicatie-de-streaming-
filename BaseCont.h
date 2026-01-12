54bdaf5d4c3adb25d95d65e398b05f661b5635dc
annebrt <153097284+annebrt@users.noreply.github.com>
2026-01-12 05:35:49 +0200

    Update BaseCont.h

--- BaseCont.h
+++ BaseCont.h
@@ -10,14 +10,47 @@ class BaseCont{
     protected: 
 
         std::list<char*> preferinte; 
-        std::list<BaseVideo*> istoric;
+        std::list<const BaseVideo*> istoric;
         int puncte;
 
-        public: BaseCont(const std::list<char*> preferinte){
+        public: BaseCont(const std::list<char*> preferinte):puncte(0){
 
-            this->preferinte=preferinte;
+            for(auto preferinta:preferinte){
+
+                char* pref= new char[std::strlen(preferinta)+1];
+                std::strcpy(pref,preferinta);
+                this->preferinte.push_back(pref);
+
+            }
 
         }
+
+        const std::list<char*>& getPreferinte() const {
+            return this->preferinte;
+        }
+
+        const std::list<const BaseVideo*>& getIstoric() const {
+
+            return this->istoric;
+
+        }
+
+        void setIstoric(const std::list<const BaseVideo*>& istoric) {
+            this->istoric=istoric;
+        }
+
+    
+    virtual ~BaseCont(){
+
+        for(auto preferinta:preferinte){
+            delete []preferinta;
+        }
+        preferinte.clear();
+
+    }
+
+    virtual void oferaInformatii()=0; 
+
 
 };
 
