b1dad477d083b5e5bf7c3e965bc13575275f8448
annebrt <153097284+annebrt@users.noreply.github.com>
2026-01-12 05:37:58 +0200

    Update Episod.h

--- Episod.h
+++ Episod.h
@@ -3,6 +3,7 @@
 
 #include "BaseVideo.h"
 #include "Durata.h"
+#include <ctime>
 
 class Episod: public BaseVideo{
 
@@ -19,14 +20,76 @@ class Episod: public BaseVideo{
 
         }
 
-    public:
 
-        Episod(const char* denumire, std::list<std::string> cast, const char* genre, const char* descriere,Durata* durata):BaseVideo(denumire,cast,genre,descriere),durata(durata){}
+        void citire(std::istream& in) override{
+
+            BaseVideo::citire(in);
+
+            Durata* durata= new Durata();
+
+            in>>*durata;
+
+            this->durata= durata;
+
+
+        }
+
+    public:
+        
+        Episod():BaseVideo(),durata(nullptr){}
+
+        Episod(const char* denumire, std::list<std::string> cast, const char* genre, const char* descriere,Durata* durata):BaseVideo(denumire,cast,genre,descriere){
+
+            this->durata=new Durata(*durata);
+
+        }
         
         
+    ~Episod(){
+
+        delete durata;
+
+    }
+
+        Episod(const Episod& other):BaseVideo(other.denumire,other.cast,other.genre,other.descriere){
+
+            Durata* durata=  new Durata(*other.durata);
+
+            this->durata=durata;
+
+        }
 
 
 
 };
+
+class Clock{
+
+    private:
+        Clock(){}
+
+        Clock(Clock& other)= delete;
+
+        Clock& operator=(Clock& other)=delete;
+
+    public:
+    
+    static Clock& getInstance(){
+
+        static Clock clock;
+        return clock;
+
+    }
+
+    std::time_t now(){
+
+        return std::time(nullptr);
+
+    }
+
+    
+
+};
+
 
 #endif // EPISOD_H
